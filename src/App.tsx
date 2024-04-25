db353b4940e5f9a5998992a5f14527d3753605db
Selim <93911103+Selim96@users.noreply.github.com>
2024-04-25 11:54:50 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const App: React.FC = () => {
+  
+  return (
+    <div className="app">
+      <h1 className='title'>My Todos</h1>
+        
+      {<p className='propouse'>Add new Todo</p>}
+    </div>
+  )
+}
+
+export default App
